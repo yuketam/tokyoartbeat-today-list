b6b2c84b857ce56770d672b30175e4d9e67e60c0
yuketam <yuketam@gmail.com>
2016-09-16 18:31:54 +0900

    refactoring

--- src/fetcher.js
+++ src/fetcher.js
@@ -3,17 +3,22 @@ import axios from 'axios';
 
 // add local cache
 
+const promisedParseString = (data) => {
+  return new Promise((resolve, reject) => {
+    parseString(data.data, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+};
+
 export function getListByRegion(region) {
   const url = `http://www.tokyoartbeat.com/events/xml.php?lang=ja&contentType=${region}`;
   return axios
     .get(url)
-    .then(response => {
-      return new Promise((resolve, reject) => {
-        parseString(response.data, (err, res) => {
-          err ? reject(err) : resolve(res);
-        });
-      });
-    })
+    .then(response => promisedParseString(response))
     .catch(err => {
       callback(err);
     });
