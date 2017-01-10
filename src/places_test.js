3864850cadb8cbe28d45ddef9b7af9cf18d4331b
yuketam <yuketam@gmail.com>
2017-01-10 18:55:27 +0900

    add tests

---
+++ src/places_test.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import axios from 'axios';
+import * as fetcher from './fetcher';
+
+const area = 'KiyosumiRyogoku';
+
+const getAreaName = (item) => {
+  return _.get(item, 'fullname_ja');
+};
+
+fetcher.getPlaceListByRegion(area)
+  .then((res) => {
+    console.log('');
+    console.log('area name', area);
+    console.log('');
+
+    res.venues.forEach((item, i) => {
+      console.log('area_id', getAreaName(item));
+    });
+  })
+  .catch((err) => {
+    console.log('error!', area, err);
+  });
